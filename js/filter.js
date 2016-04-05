0a7175fce194911ce2780dadf7d402a8570ce477
Balint Junkuncz <balint.junkuncz@cheppers.com>
2016-04-05 12:05:28 +0200

    Import widgets added.

--- js/filter.js
+++ js/filter.js
@@ -13,31 +13,89 @@
 
 // Create a vclick event which is working on mobile and desktop. It's fired on
 // click and touch events as well.
-(function ($, Drupal, window, document) {
+(function ($, Drupal) {
   'use strict';
 
-  $(document).ready(function () {
-    $('.form-select-import .form-item-project').append(" <label for='form-select-status'>Status</label>  <select id='form-select-status' class='form-select'>");
-    $('.form-select-import .form-item-project').append(" <label for='form-select-search'>Search</label>  <input placeholder='Filter by Item Name' type='text' id='form-select-search' class='form-text'  maxlength='255' size='60' value=''>");
+  Drupal.behaviors.gcImportFilter = {
+    attach: function (context, settings) {
 
-    $("#form-select-search").keyup(function(){
+      $("#edit-import table:not(.sticky-header)").once('gc-import-filter', function(){
+        $('.gc-import-filters').remove();
 
-      // Retrieve the input field text and reset the count to zero.
-      var filter = $(this).val(), count = 0;
+        $('.form-select-import .form-item-project').append(" <div class='gc-import-filters project-status'><label for='ga-form-select-status'>Status</label>  <select id='ga-form-select-status' class='form-select'></select></div>");
+        $('.form-select-import .form-item-project').append(" <div class='gc-import-filters'><label for='ga-form-select-template'>GatherContent Template Name</label>  <select id='ga-form-select-template' class='form-select'></div>");
 
-      // Loop through the table.
-      $("#edit-import table tbody tr").each(function(){
+        $('.form-select-import .form-item-project').append(" <div class='gc-import-filters'><label for='ga-form-select-search'>Search</label>  <input placeholder='Filter by Item Name' type='text' id='ga-form-select-search' class='form-text'  maxlength='255' size='60' value=''></div>");
 
-        // If the list item does not contain the text phrase fade it out.
-        if ($(this).text().search(new RegExp(filter, "i")) < 0) {
-          $(this).fadeOut();
+        $("#ga-form-select-search").keyup(function(){
 
-          // Show the list item if the phrase matches and increase the count by 1.
-        } else {
-          $(this).show();
-          count++;
-        }
+          // Retrieve the input field text and reset the count to zero.
+          var filter = $(this).val(), count = 0;
+
+          // Loop through the table.
+          $("#edit-import table tbody tr").each(function(){
+
+            // If the list item does not contain the text phrase fade it out.
+            if ($(this).text().search(new RegExp(filter, "i")) < 0) {
+              $(this).fadeOut();
+
+              // Show the list item if the phrase matches and increase the count by 1.
+            } else {
+              $(this).show();
+              count++;
+            }
+          });
+        });
+
+        $("#edit-import table tbody .status-item").each(function(){
+          var optionText =  $(this).text();
+          var optionvalue = optionText.toLowerCase().replace(/[^a-z0-9]/g, '-');
+          $(this).closest('tr').attr('data-status', optionvalue);
+          if ($('#ga-form-select-status option[value="' + optionvalue + '"]').length === 0 ) {
+            $('#ga-form-select-status').append("<option value='" + optionvalue  + "'>" + optionText + "</option>");
+          }
+          console.log($(this).text());
+          console.log($('#ga-form-select-status option[value="' + optionvalue + '"]').length);
+        });
+
+        $("#edit-import .template-name-item").each(function(){
+          var optionText =  $(this).text();
+          var optionvalue = optionText.toLowerCase().replace(/[^a-z0-9]/g, '-');
+          $(this).closest('tr').attr('data-template', optionvalue);
+          if ($('#ga-form-select-template option[value="' + optionvalue + '"]').length === 0 ) {
+            $('#ga-form-select-template').append("<option value='" + optionvalue  + "'>" + optionText + "</option>");
+          }
+        });
       });
-    });
-  });
-})(jQuery);
+
+      $('#ga-form-select-status').bind('change', function() {
+
+        $(".gc-import-filter-processed .selected").each(function(){
+          $(this).removeClass('selected');
+        });
+        $(".gc-import-filter-processed input[type='checkbox']").each(function(){
+          $(this).attr( "checked", false )
+        });
+        var selectedValue = $(this).val();
+        console.log(selectedValue);
+        $("[data-status]").hide();
+        $("[data-status='" + selectedValue + "']").show();
+      });
+
+      $('#ga-form-select-template').bind('change', function() {
+
+        $(".gc-import-filter-processed .selected").each(function(){
+          $(this).removeClass('selected');
+        });
+        $(".gc-import-filter-processed input[type='checkbox']").each(function(){
+          $(this).attr( "checked", false )
+        });
+        var selectedValue = $(this).val();
+        console.log(selectedValue);
+        $("[data-template]").hide();
+        $("[data-template='" + selectedValue + "']").show();
+      });
+
+    }
+  };
+})(jQuery, Drupal);
