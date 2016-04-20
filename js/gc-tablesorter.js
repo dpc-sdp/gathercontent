e77c1183089489d60029bff644abddb4823efa34
Zoltan Horvath <zoltan.horvath@cheppers.com>
2016-04-20 15:34:51 +0200

    Issue #2712391 by huzooka: refs #54 – Fixes date sorting in gc_tableselect form elements.

--- js/gc-tablesorter.js
+++ js/gc-tablesorter.js
@@ -8,6 +8,28 @@
 
   Drupal.behaviors.gcTableSorter = {
     attach: function (context) {
+      if (typeof $.tablesorter !== 'undefined') {
+        $.tablesorter.addParser({
+          // Setting a unique id.
+          id: 'datadate',
+          is: function (s, table, cell, $cell) {
+            if ($cell.attr('data-date')) {
+              return true;
+            }
+            return false;
+          },
+          format: function (s, table, cell, cellIndex) {
+            var $cell = $(cell);
+            if ($cell.attr('data-date')) {
+              return $cell.attr('data-date') || s;
+            }
+            return s;
+          },
+          parsed: false,
+          type: 'text'
+        });
+      }
+
       $('table.tablesorter-enabled', context).once('gc-tablesorter', function () {
         $(this).tablesorter({
           cssAsc: 'sort-down',
