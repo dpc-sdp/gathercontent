7d65cb2a9f2c76a95d82164c76127dfe6a39a75a
Roland Kovacsics <roland.kovacsics@cheppers.com>
2017-08-02 10:07:30 +0200

    Issue #2899058 by Kova101: Move upload to its own module.

--- src/Event/GatherContentEvents.php
+++ src/Event/GatherContentEvents.php
@@ -44,37 +44,6 @@ final class GatherContentEvents {
   const POST_NODE_SAVE = 'gathercontent.post_node_save';
 
   /**
-   * Name of the event fired before we post node to GatherContent.
-   *
-   * This event allows modules to perform an action before node is uploaded
-   * from Drupal to GatherContent. The event listener method receives
-   * a \Drupal\gathercontent\Event\PreNodeUploadEvent instance.
-   *
-   * @Event
-   *
-   * @see \Drupal\gathercontent\Event\PreNodeUploadEvent
-   *
-   * @var string
-   */
-  const PRE_NODE_UPLOAD = 'gathercontent.pre_node_upload';
-
-  /**
-   * Name of the event fired after we post node to GatherContent.
-   *
-   * This event allows modules to perform an action after node is uploaded
-   * to GatherContent from Drupal. The event is triggered only after successful
-   * upload. The event listener method receives
-   * a \Drupal\gathercontent\Event\PostNodeSaveEvent instance.
-   *
-   * @Event
-   *
-   * @see \Drupal\gathercontent\Event\PostNodeUploadEvent
-   *
-   * @var string
-   */
-  const POST_NODE_UPLOAD = 'gathercontent.post_node_upload';
-
-  /**
    * Name of the event fired after we save node from GatherContent.
    *
    * This event allows modules to perform an action after selected nodes are
@@ -88,20 +57,5 @@ final class GatherContentEvents {
    * @var string
    */
   const POST_IMPORT = 'gathercontent.post_import';
-
-  /**
-   * Name of the event fired after we post node to GatherContent.
-   *
-   * This event allows modules to perform an action after selected nodes are
-   * uploaded to GatherContent from Drupal. The event listener method receives
-   * a \Drupal\gathercontent\Event\PostUploadEvent instance.
-   *
-   * @Event
-   *
-   * @see \Drupal\gathercontent\Event\PostUploadEvent
-   *
-   * @var string
-   */
-  const POST_UPLOAD = 'gathercontent.post_upload';
 
 }
